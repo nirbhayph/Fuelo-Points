5db684d42413f72a52c9b6cd5da1ca77d224ff1c
nirbhayph <pherwani37@gmail.com>
2016-10-29 20:56:10 +0530

    Commit 11 - Registered User and store details to server

---
+++ app/src/main/java/com/example/dhirenchandnani/fuelo/RegisterUserActivity.java
@@ -0,0 +1,126 @@
+package com.example.dhirenchandnani.fuelo;
+
+/**
+ * Created by Dhiren Chandnani on 29-10-2016.
+ */
+
+import android.app.AlertDialog;
+import android.os.AsyncTask;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
+
+public class RegisterUserActivity  extends AsyncTask<String,Void,String> {
+    ProgressDialog am;
+    private Context context;
+    //flag 0 means get and 1 means post.(By default it is get.)
+    public RegisterUserActivity(Context context) {
+        this.context = context;
+
+
+    }
+
+    protected void onPreExecute(){
+        am = new ProgressDialog(this.context, AlertDialog.THEME_HOLO_DARK);
+        am.setTitle("Registering User");
+        am.setMessage("Registering ... ");
+        am.show();
+        Log.e("onPreExecutive", "called" + am);
+    }
+
+    @Override
+    protected String doInBackground(String... arg0) {
+
+
+        try{
+            String cars_count = arg0[0];
+            String cars_name = arg0[1];
+            String cars_type = arg0[2];
+            String name = arg0[3];
+            String pass = arg0[4];
+            String email = arg0[5];
+
+
+            String link="http://109.73.164.163/FueloPoints/server_files/user_registeration.php";
+            String data  = URLEncoder.encode("car_count", "UTF-8") + "=" + URLEncoder.encode(cars_count, "UTF-8");
+            data += "&" + URLEncoder.encode("car_names", "UTF-8") + "=" + URLEncoder.encode(cars_name, "UTF-8");
+            data += "&" + URLEncoder.encode("car_types", "UTF-8") + "=" + URLEncoder.encode(cars_type, "UTF-8");
+            data += "&" + URLEncoder.encode("name", "UTF-8") + "=" + URLEncoder.encode(name, "UTF-8");
+            data += "&" + URLEncoder.encode("pass", "UTF-8") + "=" + URLEncoder.encode(pass, "UTF-8");
+            data += "&" + URLEncoder.encode("email", "UTF-8") + "=" + URLEncoder.encode(email, "UTF-8");
+
+
+
+            URL url = new URL(link);
+            URLConnection conn = url.openConnection();
+
+            conn.setDoOutput(true);
+            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+
+            wr.write( data );
+            wr.flush();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+
+            // Read Server Response
+            while((line = reader.readLine()) != null)
+            {
+                sb.append(line);
+                break;
+            }
+            return sb.toString();
+        }
+        catch(Exception e){
+            return new String("Exception: " + e.getMessage());
+        }
+
+    }
+
+    @Override
+    protected void onPostExecute(String result){
+        am.dismiss();
+
+        if(result != null && !result.isEmpty()){
+            if(result.equals("Success")) {
+                Intent intent = new Intent(this.context, LoginActivity.class);
+                this.context.startActivity(intent);
+            }
+            Toast toast= Toast.makeText(this.context, "Registered Successfully!", Toast.LENGTH_SHORT);
+            toast.setMargin(150,150);
+            toast.show();
+
+        }
+        else{
+            Toast toast= Toast.makeText(this.context, "Unable to connect to the server!", Toast.LENGTH_SHORT);
+            toast.setMargin(150,150);
+            toast.show();
+        }
+
+
+
+    }
+}
+
--- app/src/main/java/com/example/dhirenchandnani/fuelo/SignUpActivity.java
+++ app/src/main/java/com/example/dhirenchandnani/fuelo/SignUpActivity.java
@@ -1,11 +1,13 @@
 package com.example.dhirenchandnani.fuelo;
 
+import android.content.ClipData;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 
 /**
@@ -86,4 +88,45 @@ public class SignUpActivity extends AppCompatActivity {
             }
                 c--;
     }
+
+    public void registerUser(View view){
+        String cars_list_name="";
+        String cars_list_type="";
+        EditText et1,et2;
+        for(int i=0;i<c;i++){
+            int ed1id = getResources().getIdentifier("ed"+(i+1)+"a", "id", getPackageName());
+            int ed2id = getResources().getIdentifier("ed"+(i+1)+"b", "id", getPackageName());
+            et1 = (EditText) findViewById(ed1id);
+            et2 = (EditText) findViewById(ed2id);
+            cars_list_name+= et1.getText().toString()+"///";
+            cars_list_type+= et2.getText().toString()+"///";
+
+        }
+        EditText name = (EditText) findViewById(R.id.name_reg);
+        String name_ = name.getText().toString();
+
+        EditText email = (EditText) findViewById(R.id.email_reg);
+        String email_ = email.getText().toString();
+
+        EditText password = (EditText) findViewById(R.id.password_reg);
+        String password_ = password.getText().toString();
+
+        EditText reppass = (EditText) findViewById(R.id.reppassword_reg);
+        String reppass_ = reppass.getText().toString();
+
+        if(password_.equals(reppass_)) {
+
+            new RegisterUserActivity(this).execute(c+"", cars_list_name, cars_list_type, name_, password_, email_);
+        }
+        else{
+            Toast toast= Toast.makeText(this, "Passwords Don't Match!", Toast.LENGTH_SHORT);
+            toast.setMargin(150,150);
+            toast.show();
+
+        }
+
+
+    }
+
+
 }
