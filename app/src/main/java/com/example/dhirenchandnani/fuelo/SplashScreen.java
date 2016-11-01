c8cf21283475588e4d28a8ba1eee99775ea13704
nirbhayph <pherwani37@gmail.com>
2016-11-01 15:53:14 +0530

    Commit 29-Splash Screen done

---
+++ app/src/main/java/com/example/dhirenchandnani/fuelo/SplashScreen.java
@@ -0,0 +1,55 @@
+package com.example.dhirenchandnani.fuelo;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.widget.TextView;
+
+/**
+ * Created by nirbhay on 11/1/16.
+ */
+
+public class SplashScreen extends AppCompatActivity{
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.splash_screen);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().hide();
+        //Added font Pacifico to Splash Screen
+        TextView title = (TextView) findViewById(R.id.label_points1);
+        Typeface font = Typeface.createFromAsset(this.getAssets(), "fonts/Pacifico.ttf");
+        title.setTypeface(font);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(8500);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,LoginActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+
+
+
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
