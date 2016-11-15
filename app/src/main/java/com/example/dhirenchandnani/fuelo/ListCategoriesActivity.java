b6b4716c1a0986ef060a77db8842d390cc263b82
nirbhayph <pherwani37@gmail.com>
2016-11-15 19:51:41 +0530

    Commit 40 - Added Slider with URL option

--- app/src/main/java/com/example/dhirenchandnani/fuelo/ListCategoriesActivity.java
+++ app/src/main/java/com/example/dhirenchandnani/fuelo/ListCategoriesActivity.java
@@ -6,6 +6,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.DefaultSliderView;
+import com.daimajia.slider.library.Tricks.ViewPagerEx;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -16,9 +23,10 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class ListCategoriesActivity extends AppCompatActivity {
+public class ListCategoriesActivity extends AppCompatActivity implements BaseSliderView.OnSliderClickListener, ViewPagerEx.OnPageChangeListener{
 
     public String[] category_list;
+    private SliderLayout mDemoSlider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +35,46 @@ public class ListCategoriesActivity extends AppCompatActivity {
         Log.d("Entered LCA","");
         getJSON();
 
+        /* For Slider */
+        mDemoSlider = (SliderLayout)findViewById(R.id.slider);
 
 
+        mDemoSlider.setPresetTransformer(SliderLayout.Transformer.Accordion);
+        mDemoSlider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+        mDemoSlider.setCustomAnimation(new DescriptionAnimation());
+        mDemoSlider.setDuration(4000);
+        mDemoSlider.addOnPageChangeListener(this);
+
+        DefaultSliderView dSliderView1 = new DefaultSliderView(this);
+        dSliderView1
+                .image("http://images.boomsbeat.com/data/images/full/19640/game-of-thrones-season-4-jpg.jpg");
+        mDemoSlider.addSlider(dSliderView1);
+
+        DefaultSliderView dSliderView2 = new DefaultSliderView(this);
+        dSliderView2
+                .image("http://83e2u32cf1b4dlzbl29etyxt.wpengine.netdna-cdn.com/wp-content/uploads/2014/06/banner.png");
+        mDemoSlider.addSlider(dSliderView2);
+
+        DefaultSliderView dSliderView3 = new DefaultSliderView(this);
+        dSliderView3
+                .image("http://www.f-covers.com/cover/friends-4-facebook-cover-timeline-banner-for-fb.jpg");
+        mDemoSlider.addSlider(dSliderView3);
+
+        DefaultSliderView dSliderView4 = new DefaultSliderView(this);
+        dSliderView4
+                .image("http://orig06.deviantart.net/b675/f/2013/110/e/6/arrow_facebook_banner_by_cre5po-d62fdtu.jpg");
+        mDemoSlider.addSlider(dSliderView4);
+
+        DefaultSliderView dSliderView5 = new DefaultSliderView(this);
+        dSliderView5
+                .image("http://img11.deviantart.net/46ad/i/2011/022/8/4/sherlock_banner_by_danglingdingle-d37r6ov.jpg");
+        mDemoSlider.addSlider(dSliderView5);
+
+
+        mDemoSlider.setPresetTransformer(5);
+        mDemoSlider.setDuration(20);
+
+        /* Slider Creation Ends*/
 
     }
 
@@ -121,6 +167,33 @@ public class ListCategoriesActivity extends AppCompatActivity {
         }
         return category;
     }
+
+    @Override
+    protected void onStop() {
+        // To prevent a memory leak on rotation, make sure to call stopAutoCycle() on the slider before activity or fragment is destroyed
+        mDemoSlider.stopAutoCycle();
+        super.onStop();
+    }
+
+    @Override
+    public void onSliderClick(BaseSliderView slider) {
+        Toast.makeText(this,slider.getBundle().get("extra") + "",Toast.LENGTH_SHORT).show();
+    }
+
+
+
+
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+
+    @Override
+    public void onPageSelected(int position) {
+        Log.d("Slider Demo", "Page Changed: " + position);
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {}
 
 
 
